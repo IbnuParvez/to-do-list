e872e42cbdc9e4c8289d86d6161570f347e2283b
IbnuParvez <parvezfatma98@gmail.com>
2026-04-07 22:56:41 +0300

    Render project header and todos in DOM
    Make loadPages accept a project and populate #content with a
    header and a todo entry (div + h3) for each project.todos
    Move styles.css import from dom.js to index.js

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,17 @@
-import "./styles.css";
-
-export default function loadPages() {
+function loadPages(project) {
   const content = document.getElementById("content");
   content.innerHTML = "";
+
+  const header = document.createElement("h1");
+  header.textContent = project.name;
+
+  content.appendChild(header);
+  project.todos.forEach((todo) => {
+    const div = document.createElement("div");
+    const title = document.createElement("h3");
+    title.textContent = todo.title;
+    div.appendChild(title);
+    content.appendChild(div);
+  });
 }
+export default loadPages;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import ToDo from "./to-do.js";
 import Projects from "./project.js";
 import { saveStorage, loadFromStorage } from "./storage.js";
 import loadPages from "./dom.js";
+import "./styles.css";
+
 const allProjects = [];
